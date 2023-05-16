2c5caf8c4c4f5fbb984e5b6188609c594ca4ce1c
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-05-16 18:33:31 +0000

    key

--- lib/services/ai_handler.dart
+++ lib/services/ai_handler.dart
@@ -19,8 +19,9 @@ class AIHandler {
       currentSession.clear();
     }
   }
+
   final _openAI = OpenAI.instance.build(
-    token: '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',
+    token: '',
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
@@ -32,14 +33,14 @@ class AIHandler {
       final request = ChatCompleteText(messages: [
         Map.of({"role": "user", "content": message})
       ], maxToken: 200, model: ChatModel.gptTurbo0301);
-        final rep = request.messages[0].values;
+      final rep = request.messages[0].values;
       final response = await _openAI.onChatCompletion(request: request);
-    if (response != null) {
-      String reply = response.choices[0].message!.content.trim();
-      List<String> session = [message, reply];
-      await _databaseManager.saveSession(session);
-      return reply;
-    }
+      if (response != null) {
+        String reply = response.choices[0].message!.content.trim();
+        List<String> session = [message, reply];
+        await _databaseManager.saveSession(session);
+        return reply;
+      }
 
       return 'Some thing went wrong';
     } catch (e) {
@@ -50,7 +51,6 @@ class AIHandler {
   Future<List<List<String>>> getConversationSessions() async {
     return _databaseManager.getAllSessions();
   }
-
 
   void dispose() {
     _openAI;
