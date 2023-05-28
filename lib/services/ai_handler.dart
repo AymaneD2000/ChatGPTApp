cbbe4d0096a66466ea3cd4a680677531247b2c76
Babry Galledou <babrygalledou@Babrys-MacBook-Pro.local>
2023-05-28 19:57:05 +0000

    chatdesign

--- lib/services/ai_handler.dart
+++ lib/services/ai_handler.dart
@@ -28,7 +28,7 @@ class AIHandler {
   }
 
   final _openAI = OpenAI.instance.build(
-    token: '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',
+    token: '',
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
@@ -43,10 +43,7 @@ class AIHandler {
       list.addAll(newList);
     }
     try {
-      print(list.length);
-
       list.add({"role": "user", "content": message});
-      print(list.length);
 
       //Map.of({"role": "user", "content": message})
       final request = ChatCompleteText(
@@ -54,23 +51,21 @@ class AIHandler {
       //final rep = request.messages[0].values;
       final response = await _openAI.onChatCompletion(request: request);
 
-      print(response);
       if (response != null) {
         String reply = response.choices[0].message!.content.trim();
         List<String> session = [message, reply];
-        print("longueur de la liste lors du get ${list.length}");
 
         //Tranformer la liste des discussions de cette session en Json pour pouvoir stocker
         //la liste tout entier dans sqflite
 
         String listToJson = jsonEncode(list);
-        print("La liste en format Json $listToJson");
+
         //mise a jour de la liste dans la base de donnees
         await _databaseManager.deleteSessionListe(sessionId);
         await _databaseManager.saveSessionListe(listToJson, sessionId);
         //var lister = await _databaseManager.getAllSessionsListe(sessionId);
         list.add({"role": "assistant", "content": reply});
-        print("longueur de la liste avant get ${list.length}");
+
         await _databaseManager.saveSession(session, sessionId);
 
         return reply;
